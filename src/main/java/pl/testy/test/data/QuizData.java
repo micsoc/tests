4297bc4070f6437f534a03cbbc4b864e836afec3
Michal Socha <michal.socha.88@gmail.com>
2020-02-02 17:18:32 +0100

    usuniete pytania

--- src/main/java/pl/testy/test/data/QuizData.java
+++ src/main/java/pl/testy/test/data/QuizData.java
@@ -16,22 +16,5 @@ public class QuizData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-        Test test = new Test("About everything");
-        Question question = new Question("What is The Capital of Poland");
-        question.addAnswer(new Answer("Warsaw",true));
-        question.addAnswer(new Answer("Mumbai",false));
-        question.addAnswer(new Answer("Kolkatta",false));
-        Question question3 = new Question("Name World's Richest Man?");
-        question3.addAnswer(new Answer("Jeff Bazo",false));
-        question3.addAnswer(new Answer("Bill Gates",false));
-        question3.addAnswer(new Answer("Mark Zuckenberg",true));
-        Question question4 = new Question("Word's Longest River?");
-        question4.addAnswer(new Answer("River Nile",true));
-        question4.addAnswer(new Answer("River Amazon",false));
-        question4.addAnswer(new Answer("River Godavari",false));
-        test.addQuestion(question);
-        test.addQuestion(question3);
-        testRepository.save(test);
     }
 }
